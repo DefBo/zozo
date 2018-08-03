a10082bca0cd7d5181a8e16dd7b1f1bc83a6a53a
Bogdan Andriiv <35779282+DefBo@users.noreply.github.com>
2018-08-04 00:49:14 +0300

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -1,15 +1,15 @@
 $(function() {
 
 	// Preloader function
-	$(window).load = function() {
-		setTimeout(function() {
-			var preloader = document.getElementById('preloader');
-			if( !preloader.classList.contains('done') )
-			{
-				preloader.classList.add('done');
-			}
-		}, 1000);
-	};
+	// $(window).load = function() {
+	// 	setTimeout(function() {
+	// 		var preloader = document.getElementById('preloader');
+	// 		if( !preloader.classList.contains('done') )
+	// 		{
+	// 			preloader.classList.add('done');
+	// 		}
+	// 	}, 1000);
+	// };
 
 	$(document).ready(function(){
 		$('.mainpage_navicon').click(function(){
